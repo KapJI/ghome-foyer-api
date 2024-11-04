b7a7da139a8e33d818bdae07d0a8f217bd7808f5
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-11-04 23:11:02 +0000

    Migrate from poetry to uv

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,6 +4,6 @@ set -e
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd "$SCRIPT_DIR/.."
-poetry install
-poetry run python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. --mypy_out=readable_stubs:. --mypy_grpc_out=readable_stubs:.  ghome_foyer_api/api.proto
-poetry run pre-commit run --hook-stage manual --all-files
+uv sync
+uv run python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. --mypy_out=readable_stubs:. --mypy_grpc_out=readable_stubs:.  ghome_foyer_api/api.proto
+uv run pre-commit run --hook-stage manual --all-files
