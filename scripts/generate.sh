61b0cad64c8af4389ebd36b6a35ed6bf8ac2826f
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-11-05 10:59:27 +0000

    Regenerate stubs without 'readable_stubs:'

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,5 +5,5 @@ set -e
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd "$SCRIPT_DIR/.."
 uv sync
-uv run python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. --mypy_out=readable_stubs:. --mypy_grpc_out=readable_stubs:.  ghome_foyer_api/api.proto
+uv run python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. --mypy_out=readable_stubs:. --mypy_grpc_out=.  ghome_foyer_api/api.proto
 uv run pre-commit run --hook-stage manual --all-files
