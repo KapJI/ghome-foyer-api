456838a72755f336fca765adb7216a902d98a565
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2025-05-29 12:46:18 +0100

    Allow protobuf v6

--- ghome_foyer_api/api_pb2_grpc.py
+++ ghome_foyer_api/api_pb2_grpc.py
@@ -7,7 +7,7 @@ import grpc
 
 from ghome_foyer_api import api_pb2 as ghome__foyer__api_dot_api__pb2
 
-GRPC_GENERATED_VERSION = "1.66.2"
+GRPC_GENERATED_VERSION = "1.71.0"
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
